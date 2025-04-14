7463c6a52e1292e97e1bed3227592e03a9eef8f3
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-04-14 22:00:49 +0200

    Add Profile and Token models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
+from datetime import timezone
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+class SpotifyToken(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
+    token_expires = models.DateTimeField()
+    token_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.user.username}'s Spotify Token"
+    
+    def is_valid(self):
+        return self.token_expires > timezone.now()
