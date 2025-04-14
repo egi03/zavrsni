0d7b61fc55a313625a13e3c5b3ef5d00f6d61777
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-04-14 23:31:09 +0200

    Add login and logout urls

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
