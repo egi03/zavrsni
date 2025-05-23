47fabdbd4577dfd753aeadd55e8a712afe5be533
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-05-24 00:19:37 +0200

    Add url for profile pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('profile/<str:username>', views.profile_view, name='profile_view'),
 ]
