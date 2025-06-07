ad585e588b91a2b3cbdd0ada007abc9d54196830
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-06-07 11:21:24 +0200

    Add endpoint for searching profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('profile/<str:username>', views.profile_view, name='profile_view'),
+    path('profile/<str:username>/', views.profile_view, name='profile_view'),
+    path('search-profiles/', views.search_profiles, name='search_profiles'),
 ]
